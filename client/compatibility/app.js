7f9bc912f633fe07c1bdc18f9c6ccb794027383d
Jon Perl <perl.jonathan@gmail.com>
2014-03-24 10:55:07 -0400

    Init commit

---
+++ client/compatibility/app.js
@@ -0,0 +1,39 @@
+//  CodePen Evaluation License
+//  
+//  Copyright (c) 2013 Famous Industries, Inc.
+//  
+//  Non-sublicensable permission is hereby granted, free of charge, to any person obtaining a 
+//  copy of this software and associated documentation files directly from codepen.io (the 
+//  "Software"), solely to internally make and internally use copies of the Software to test, 
+//  explore, and evaluate the Software solely in such personÃ¢â‚¬â„¢s non-commercial, non-
+//  production environments, provided that the above copyright notice and this permission 
+//  notice shall be included in all copies or substantial portions of the Software. 
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+//  MERCHANTABILITY, FITNESS FOR A ARTICULAR PURPOSE AND 
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+//  SOFTWARE.
+//
+
+
+Famous(function(require,exports,module)
+{
+    var Engine          = require('famous/Engine');
+    var Modifier        = require('famous/Modifier');
+
+    var FM              = require('famous/Matrix');
+    var Easing          = require('famous-animation/Easing');
+
+    var AppView         = require('./app/AppView');
+
+    var Context = Engine.createContext();
+
+    var appView = new AppView();
+
+    Context.link(appView);
+    Context.setPerspective(2000);
+});
